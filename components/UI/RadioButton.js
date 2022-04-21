1b6c06a6c3369cb15d349327a8fe90a2cdb44592
Ivan Avvakumov <66466689+avvakumovid@users.noreply.github.com>
2022-04-21 14:50:24 +0300

    Добавдение переключений маршрута

---
+++ components/UI/RadioButton.js
@@ -0,0 +1,79 @@
+import React, { Component, useState } from "react";
+import { View, TouchableOpacity, Text, StyleSheet } from 'react-native';
+
+
+
+const RadioButton = ({prop, setRoute}) => {
+
+    const [value, setValue] = useState()
+    return (
+      <>
+      <Text style={styles.heading}>Routes</Text>
+      <View style={styles.main}>
+        {prop.map(res => {
+          return (
+            <View key={res.id} style={styles.container}>
+              <Text style={styles.radioText}>{res.id}</Text>
+              <TouchableOpacity
+                style={styles.radioCircle}
+                onPress={() => {
+                  setValue(res.id);
+                  setRoute(res.route)
+                }}>
+                {value === res.id && <View style={styles.selectedRb} />}
+              </TouchableOpacity>
+            </View>
+          );
+        })}
+      </View>
+      </>
+    )
+
+}
+const styles = StyleSheet.create({
+  heading: {
+    textAlign: "center",
+    marginBottom: 5,
+    fontSize: 18
+  },
+  main: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-around",
+    marginBottom: 5
+  },
+  container: {
+    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  radioText: {
+    marginRight: 5,
+    fontSize: 18,
+    color: 'grey',
+    fontWeight: '700'
+  },
+  radioCircle: {
+    height: 20,
+    width: 20,
+    borderRadius: 100,
+    borderWidth: 2,
+    borderColor: '#ccc',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  selectedRb: {
+    width: 10,
+    height: 10,
+    borderRadius: 50,
+    backgroundColor: 'grey',
+  },
+  result: {
+    marginTop: 20,
+    color: 'white',
+    fontWeight: '600',
+    backgroundColor: '#F3FBFE',
+  },
+});
+
+export default RadioButton
