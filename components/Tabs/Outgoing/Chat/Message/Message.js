10d1dbb00614470addbedb81d3ed51e8ce69f829
Ivan Avvakumov <66466689+avvakumovid@users.noreply.github.com>
2022-04-25 14:30:53 +0300

    Фикс лайнградиента

--- components/Tabs/Outgoing/Chat/Message/Message.js
+++ components/Tabs/Outgoing/Chat/Message/Message.js
@@ -1,53 +1,55 @@
 import React from 'react';
-import {ScrollView, StyleSheet, Text,} from 'react-native';
-import {LinearGradient} from 'react-native-linear-gradient';
+import {ScrollView, StyleSheet, Text} from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
 
 const Message = ({type, content}) => {
-    const colors = type === 'msg' ? ['white', 'white'] : ['#3525af', '#bf69d4']
-    return (
-        <ScrollView>
-            <LinearGradient
-                start={{x: 0, y: 0}}
-                end={{x: 1, y: 1}}
-                colors={colors}
-                style={[styles.container, type === 'msg' ? styles.msgContainer : styles.otherContainer]}>
-                <Text style={type === 'msg' ? styles.msgContent : styles.otherContent}>
-                    {content}
-                </Text>
-            </LinearGradient>
-        </ScrollView>
-
-    );
+  const colors = type === 'msg' ? ['white', 'white'] : ['#3525af', '#bf69d4'];
+  return (
+    <ScrollView>
+      <LinearGradient
+        start={{x: 0, y: 0}}
+        end={{x: 1, y: 1}}
+        colors={colors}
+        style={[
+          styles.container,
+          type === 'msg' ? styles.msgContainer : styles.otherContainer,
+        ]}
+      >
+        <Text style={type === 'msg' ? styles.msgContent : styles.otherContent}>
+          {content}
+        </Text>
+      </LinearGradient>
+    </ScrollView>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        padding: 10,
-        borderRadius: 10,
-        marginBottom: 20,
-        flexDirection: 'row'
-    },
-    wrapper: {
-        flexDirection: 'row'
-    },
-    msgContainer: {
-        backgroundColor: '#fff',
-        alignSelf: 'flex-end',
-        maxWidth: '80%',
-    },
-    otherContainer: {
-        alignSelf: 'center',
-        width: '100%',
-        padding: 25
-    },
-    msgContent: {
-        color: '#000',
-
-    },
-    otherContent: {
-        color: 'white',
-        fontSize: 16
-    }
-})
+  container: {
+    padding: 10,
+    borderRadius: 10,
+    marginBottom: 20,
+    flexDirection: 'row',
+  },
+  wrapper: {
+    flexDirection: 'row',
+  },
+  msgContainer: {
+    backgroundColor: '#fff',
+    alignSelf: 'flex-end',
+    maxWidth: '80%',
+  },
+  otherContainer: {
+    alignSelf: 'center',
+    width: '100%',
+    padding: 25,
+  },
+  msgContent: {
+    color: '#000',
+  },
+  otherContent: {
+    color: 'white',
+    fontSize: 16,
+  },
+});
 
 export default Message;
